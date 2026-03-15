baf55ebcd2a8d480017447a8d83c7ca63383c06b
bobai <bobai@duck.com>
2026-03-15 12:44:45 +0000

    chore: add Apache-2.0 license headers

--- eslint.config.js
+++ eslint.config.js
@@ -6,14 +6,18 @@
  *  ██  ▄ ██ ██   ██▄█▀ ██ ██ ██▄█▀  ███
  *   ▀█████▄▄█▀  ▄▀█▄▄▄▄█▀███▄▀█▄▄▄▄██ ██▄
  *        ▀█
- *
+ * 
  *  Copyright (C) 2026 — 2026, Qredex, LTD. All Rights Reserved.
- *
+ * 
  *  DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- *  This is proprietary and confidential. Unauthorized copying, redistributing
- *  and/or modification of this file via any medium is inexorably prohibited.
- *
+ * 
+ *  Licensed under the Apache License, Version 2.0. See LICENSE for the full license text.
+ *  You may not use this file except in compliance with that License.
+ *  Unless required by applicable law or agreed to in writing, software distributed under the
+ *  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ *  either express or implied. See the License for the specific language governing permissions
+ *  and limitations under the License.
+ * 
  *  If you need additional information or have any questions, please email: copyright@qredex.com
  */
 
